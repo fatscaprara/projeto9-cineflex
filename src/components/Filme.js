b3c459aa8f59312e04b4dd85fa94aaf7faefed73
Fabio Caprara <fats140701@gmail.com>
2022-05-23 16:35:05 -0400

    Lógica básica da renderização da tela de filmes implementada

--- src/components/Filme.js
+++ src/components/Filme.js
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 import Topo from './Topo';
+import filme267 from '../assets/img/filme-267.png';
+import filmeEnolaHolmes from '../assets/img/filme-enola-holmes.png';
 
-export default function Filme() {
+export default function Filme({ name }) {
   return (
-    <>
-      <Topo />
-      <h1>Filme</h1>
-      <Link to="/sessao">Sessao</Link>
-    </>
+    <div className="container-filme">
+      <img src={filme267} alt="" />
+      <h1>{name}</h1>
+    </div>
   );
 }
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -1,12 +1,26 @@
 import { Link } from 'react-router-dom';
+import Filme from './Filme';
 import Topo from './Topo';
 
 export default function Inicio() {
+  const filmes = [
+    'filme267',
+    'filmeEnolaHolmes',
+    'filme267',
+    'filmeEnolaHolmes',
+    'filme267',
+    'filmeEnolaHolmes',
+  ];
+
   return (
     <>
       <Topo />
-      <h1>Inicio</h1>
-      <Link to="/filme">Filme</Link>
+      <h2>Selecione o filme</h2>
+      {filmes.map((filme) => (
+        <Link to="/filme">
+          <Filme name={filme} />
+        </Link>
+      ))}
     </>
   );
 }
