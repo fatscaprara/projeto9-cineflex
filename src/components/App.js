4c5742f34be1f9cec4a0e0052bd77d8dbb27023b
Fabio Caprara <fats140701@gmail.com>
2022-07-30 11:18:00 -0400

    Assento, Dia e Filme Implementados

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Header from "./layout/Header";
+import Header from "./Header";
+import TelaAssentos from "./TelaAssentos";
+import TelaInicial from "./TelaInicial";
+import TelaSessoes from "./TelaSessoes";
+import TelaSucesso from "./TelaSucesso";
 
 function App() {
   const [reserva, setReserva] = useState(null);
@@ -8,6 +13,25 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<TelaInicial />} />
+        <Route path="/sessoes/:filmeId" element={<TelaSessoes />} />
+        <Route
+          path="/assentos/:sessaoId"
+          element={
+            <TelaAssentos finalizar={(reserva) => setReserva(reserva)} />
+          }
+        />
+        <Route
+          path="/sucesso"
+          element={
+            <TelaSucesso
+              reserva={reserva}
+              novaReserva={() => setReserva(null)}
+            />
+          }
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
